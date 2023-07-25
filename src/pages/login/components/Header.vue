da9c28026899c54ece603305a25faddfd3fab13d
baoyuhao <njr10byh@gmail.com>
2023-07-25 18:08:24 +0800

    初始化项目

---
+++ src/pages/login/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="login-header">
+    <logo-full-icon class="logo" />
+    <div class="operations-container">
+      <t-button theme="default" shape="square" variant="text" @click="navToGitHub">
+        <t-icon name="logo-github" class="icon" />
+      </t-button>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import LogoFullIcon from '@/assets/assets-logo-full.svg?component';
+
+
+const navToGitHub = () => {
+  window.open('https://github.com/tencent/tdesign-vue-next-starter');
+};
+</script>
+
+<style lang="less" scoped>
+.login-header {
+  padding: 0 var(--td-comp-paddingLR-xl);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  backdrop-filter: blur(10px);
+  color: var(--td-text-color-primary);
+  height: var(--td-comp-size-xxxl);
+
+  .logo {
+    width: 178px;
+    height: var(--td-comp-size-s);
+  }
+
+  .operations-container {
+    display: flex;
+    align-items: center;
+    .t-button {
+      margin-left: var(--td-comp-margin-l);
+    }
+  }
+}
+</style>
