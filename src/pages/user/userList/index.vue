781536ae2abb8c7253956ddbe93dd05e0f1381be
tangsangs <tangwan1024@gmail.com>
2023-08-15 10:14:11 +0800

    用户列表的查询按钮,修改

--- src/pages/user/userList/index.vue
+++ src/pages/user/userList/index.vue
@@ -228,10 +228,10 @@ const userListTable = reactive({
       registerTime: "2023-08-03 17:56:21"
     }
   ],// 表格数据
-  searchText_1: "",
-  searchText_2: "",
-  searchText_3: "",
-  searchText_4: "",
+  // searchText_1: "",
+  // searchText_2: "",
+  // searchText_3: "",
+  // searchText_4: "",
   // 表格分页
   pagination: {
     total: 0,
@@ -306,8 +306,46 @@ const qrCodeOpen = () => {
 
 const search = () => {
   // alert("查询条目失败");
+  // console.log(searchData);
+  // MessagePlugin.warning("暂未开放");
   console.log(searchData);
-  MessagePlugin.warning("暂未开放");
+  let obj = {}
+  obj = {
+    phone: searchData.phone,
+    name: searchData.name,
+    wechat:searchData.wechat,
+    statue:searchData.status,
+  }
+  console.log(searchData);
+  console.log("@",userListTable.tableData);
+  // 排除空
+  // for (let key in obj) {
+  //   if (obj[key] == '' || obj[key] == null) {
+  //     delete obj[key]
+  //   }
+  // }
+  // @param condition 过滤条件
+  // @param data 需要过滤的数据
+  let filter = (condition, data) => {
+    return data.filter(item => {
+      return Object.keys(condition).every(key => {
+        return String(item[key]).toLowerCase().includes(
+            String(condition[key]).trim().toLowerCase())
+      })
+    })
+  }
+  let data = filter(obj, userListTable.tableData);
+  console.log(data);
+  if (data != '') {
+    userListTable.tableData = data
+  } else {
+    MessagePlugin.warning(
+     `没有相关信息`,
+    );
+    data = [];
+    userListTable.tableData = data;
+  }
+
 }
 // 禁用
 const disableUser = (row: any) => {
